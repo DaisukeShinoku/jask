c9d22ecc813724f249a348bc555a57f5bf1b03a1
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2023-05-05 21:49:13 +0900

    Contextの利用

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -5,16 +5,20 @@ import Home from './component/Home'
 import Dashboard from './component/Dashboard'
 import Test from './component/Test'
 
+import { LoginProvider } from './component/providers/LoginProvider'
+
 const App = () => {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='/test' element={<Test title={'This is Test'}/>} />
-        </Routes>
-      </BrowserRouter>
+      <LoginProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/test' element={<Test title={'This is Test'}/>} />
+          </Routes>
+        </BrowserRouter>
+      </LoginProvider>
     </>
   )
 }
--- frontend/app/src/component/Home.jsx
+++ frontend/app/src/component/Home.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import { React, useContext } from 'react'
 
 import Registration from './auth/Registration'
 
+import { LoginContext } from './providers/LoginProvider'
+
 const Home = () => {
+
+  const { loggedInStatus, setLoggedInStatus } = useContext(LoginContext);
+
   return (
     <>
       <h1>Home</h1>
+      <h2>ログイン状態: {loggedInStatus ? 'ログイン済' : '未ログイン'}</h2>
       <Registration />
     </>
   )
