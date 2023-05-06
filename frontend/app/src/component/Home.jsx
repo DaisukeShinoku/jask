e1f2e1f4ca99e6d0a306aae846f5c3b2ba456632
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2023-05-06 13:18:01 +0900

    ログアウト処理

--- frontend/app/src/component/Home.jsx
+++ frontend/app/src/component/Home.jsx
@@ -2,6 +2,7 @@ import { React, useContext } from 'react'
 
 import Registration from './auth/Registration'
 import Login from './auth/Login'
+import Logout from './auth/Logout'
 
 import { LoginContext } from './providers/LoginProvider'
 
@@ -15,6 +16,7 @@ const Home = () => {
       <h2>ログイン状態: {loggedInStatus ? 'ログイン済' : '未ログイン'}</h2>
       <Registration />
       <Login />
+      <Logout />
     </>
   )
 }
---
+++ frontend/app/src/component/auth/Logout.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useContext } from 'react'
+
+import axios from 'axios'
+
+import { useNavigate } from "react-router-dom";
+
+import { LoginContext } from '../providers/LoginProvider'
+
+const Logout = () => {
+  const { setLoggedInStatus } = useContext(LoginContext);
+
+  const navigate = useNavigate(); 
+
+  const handleLogoutClick = (event) => {
+    axios.delete('http://localhost:3001/logout',
+      { withCredentials: true },
+    ).then(response => {
+      console.log('ログアウト成功', response)
+      setLoggedInStatus(false)
+      navigate('/')
+    }).catch(error => {
+      console.log('logout error', error)
+    })
+  }
+
+  return (
+    <>
+      <p>ログアウト</p>
+      <button onClick={handleLogoutClick}>ログアウト</button>
+    </>
+  )
+}
+
+export default Logout
