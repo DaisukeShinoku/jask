bf5229a4cf09788f7f44623c1514a8aa3884ff4e
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2023-05-06 11:34:31 +0900

    ログイン状態の保持

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -1,24 +1,46 @@
-import React from 'react'
+import React, { useEffect, useContext } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Home from './component/Home'
 import Dashboard from './component/Dashboard'
 import Test from './component/Test'
 
-import { LoginProvider } from './component/providers/LoginProvider'
+import { LoginContext } from './component/providers/LoginProvider'
+
+import axios from 'axios'
 
 const App = () => {
+
+  const { loggedInStatus, setLoggedInStatus, setUser } = useContext(LoginContext);
+
+  const checkLoginStatus = () => {
+    axios.get("http://localhost:3001/logged_in", { withCredentials: true })
+      .then(response => {
+        if (response.data.logged_in && loggedInStatus === false) {
+          setLoggedInStatus(true)
+          setUser(response.data.user)
+        } else if (!response.data.logged_in && loggedInStatus === true) {
+          setLoggedInStatus(false)
+          setUser({})
+        }
+      }).catch(error => {
+        console.log('ログインエラー', error)
+    })
+  }
+
+  useEffect(() => {
+    checkLoginStatus()
+  })
+
   return (
     <>
-      <LoginProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/dashboard' element={<Dashboard />} />
-            <Route path='/test' element={<Test title={'This is Test'}/>} />
-          </Routes>
-        </BrowserRouter>
-      </LoginProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/test' element={<Test title={'This is Test'}/>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
